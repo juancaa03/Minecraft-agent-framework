5aae8faf5a5b37824d157feb015b2a1bf1543ab1
73F4N <stefanoctavian.tabirca@estudiants.urv.cat>
2024-11-12 23:51:39 +0100

    borrados ficheros innecesarios y hecho que el programa funcione para varios jugadores individualmente

--- MyAdventures/botclass.py
+++ MyAdventures/botclass.py
@@ -7,8 +7,9 @@ from threading import Thread
 
 # main abstract class for a bot
 class Bot:
-    def __init__(self):
+    def __init__(self, entity):
         self.mc = game.Minecraft.create()   # minecraft game server connection
+        self.entity = entity    # player who called the function
         self.control = None     # control variable for main loop in bot function
         self.t1 = Thread        # declaration of a thread for the bot (needs to be updated by the specific bot)
     
@@ -17,30 +18,31 @@ class Bot:
         if(not self.t1.is_alive()):
             self.control = True
             self.t1.start()
-            self.mc.postToChat("***The bot has been enabled!!")
+            self.mc.postToChat(f"<{self.name}> ***The bot has been enabled for player ID {self.entity}!!")
         else:
-            self.mc.postToChat("***The bot is already running!!")
+            self.mc.postToChat(f"<{self.name}> ***The bot is already running for player ID {self.entity}!!")
     
     # main function to stop the bot thread
     def stop(self):
         if(self.t1.is_alive()):
             self.control = False
             self.t1.join()
-            self.mc.postToChat("***The bot has been disabled!!")
+            self.mc.postToChat(f"<{self.name}> ***The bot has been disabled for player ID {self.entity}!!")
         else:
-            self.mc.postToChat("***The bot is not running yet!!")
+            self.mc.postToChat(f"<{self.name}> ***The bot is not running yet for player ID {self.entity}!!")
         
         
 # specific bot class to spawn TNT near the player
 class TNT(Bot):
-    def __init__(self):
-        super().__init__()  # inherit attributes
+    def __init__(self, entity):
+        super().__init__(entity)  # inherit attributes
+        self.name = "TNTBot"    # name of this specific bot
         self.t1 = Thread(target=self._main) # update thread with the function to execute
         
     # specific function of the TNT bot
     def _main(self):
         while(self.control):    # run while the bot is enabled
-            pos = self.mc.player.getTilePos()    # Get player position
+            pos = self.mc.entity.getTilePos(self.entity)    # Get player position
             floor = self.mc.getBlock(pos.x, pos.y-1, pos.z)  # Check what block the player is standing on
             if(floor != blocks.AIR.id and floor != blocks.WATER.id and floor != blocks.WATER_STATIONARY.id):	# If the player is not flying or swimming
                     self.mc.spawnEntity(pos.x, pos.y+2, pos.z, entities.PRIMED_TNT.id)   # Spawn an ignited TNT on top of the player
--- MyAdventures/practica.py
+++ MyAdventures/practica.py
@@ -3,22 +3,62 @@ import Pyro4 as pyro
 import time
 import botclass as bots
 
+# Function to update the lists of bots, each assigned to a player (some of them)
+def updatePlayerList():
+    """Returns a list of players, followed by dictionaries of the bots that require to be
+    unique for each player in particular"""
+    list = mc.getPlayerEntityIds()  # Obtain a list of all connected players
+    
+    # For every player online, create a bot (do the same for every kind of bot)
+    TNTbotList = {entity: bots.TNT(entity) for entity in list}
+    # <<<< Add the new bot dicts here to update them as well >>>>
+    
+    return (list, TNTbotList)
+    
 
-# Connect to the Minecraft game
-mc = game.Minecraft.create()
-Script = 1
+mc = game.Minecraft.create()    # Connect to the Minecraft game
+Script = 1  # Control variable to exit program when finished
 
+
+# Player list, dictionaries for the bots that are unique to each player, and singular bots
+playerList = []
+
+TNTbotList = {}
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# * Add bot dict for every new implemented bot and include the dict in the updatePlayerList function !!!! *
+# *********************************************************************************************************
+
+playerList, TNTbotList = updatePlayerList()   	# Lists initialisation
+
+# Main program start
+mc.postToChat("<MAIN> ***Main program has started!!")
+
+# Execute until player wishes to stop it
 while(Script):
+    # If a new player joins, we want to assign them a bot so we update the lists
+    if(len(mc.getPlayerEntityIds()) != len(playerList)):
+        playerList, TNTbotList = updatePlayerList()
+    
+    # Read chat to see if anyone used a custom command
     chatEvents = mc.events.pollChatPosts()
+    
     for command in chatEvents:
-        if (command.message == ":enableTNT"):
-            TNTBot = bots.TNT()
-            TNTBot.begin()
+        text = str(command.message) # Convert chat event to str
+        if(not text.startswith(":")):   # Skip if it doesn't start with ":"
+            continue
+        
+        player = command.entityId   # Else, register what player sent the command
+        
+        # Check what command was executed (ignore case)
+        if (text.casefold() == ":enableTNT".casefold()):
+            TNTbotList[player].begin() # Start TNT bot for the player who ordered it
             
-        elif (command.message == ":disableTNT"):
-            TNTBot.stop()
+        elif (text.casefold() == ":disableTNT".casefold()):
+            TNTbotList[player].stop()   # Stop the TNT bot for the player who ordered it
+            del TNTbotList[player]  # Delete the object (thread) for this player
+            TNTbotList[player] = bots.TNT(player)   # And create a new one that is ready
             
-        elif (command.message == ":endProgram"):
-            Script = 0
+        elif (text.casefold() == ":endProgram".casefold()):
+            Script = 0  # Command to finish the execution of this program
             
-mc.postToChat("***Ended execution of main program!!")  
+mc.postToChat("<MAIN> ***Ended execution of main program!!")  
